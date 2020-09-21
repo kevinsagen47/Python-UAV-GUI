7ed973a9627f547a3313b0ca075b21e1049c74e0
kevinsagen47 <kevinsagen47@gmail.com>
2020-09-21 17:28:34 +0000

    rpi update

--- layoutV1.0.py
+++ layoutV1.0.py
@@ -25,42 +25,23 @@ filter_data = ''
 update_period = 5
 serial_object = None
 gui = Tk()
-gui.title("UART Interface")
+gui.title("UAV Command Software")
 
 
 
 
 def connect():
-    """The function initiates the Connection to the UART device with the Port and Buad fed through the Entry
-    boxes in the application.
-    The radio button selects the platform, as the serial object has different key phrases 
-    for Linux and Windows. Some Exceptions have been made to prevent the app from crashing,
-    such as blank entry fields and value errors, this is due to the state-less-ness of the 
-    UART device, the device sends data at regular intervals irrespective of the master's state.
-    The other Parts are self explanatory.
-    """
-
-    version_ = button_var.get()
-    print version_
     global serial_object
-    port = port_entry.get()
-    baud = baud_entry.get()
-    
-
+    port = 'ACM0'
+    baud = '9600'   
     try:
-        if version_ == 2:
-            try:
-                serial_object = serial.Serial('/dev/tty' + str(port), baud)
-            
-            except:
-                print "Cant Open Specified Port"
-        elif version_ == 1:
-            serial_object = serial.Serial('COM' + str(port), baud)
-
+        try:
+            serial_object = serial.Serial('/dev/tty' + str(port), baud)                
+        except:
+            print "Cant Open Specified Port"
     except ValueError:
         print "Enter Baud and Port"
         return
-
     t1 = threading.Thread(target = get_data)
     t1.daemon = True
     t1.start()
@@ -68,32 +49,18 @@ def connect():
 
 
 def get_data():
-    """This function serves the purpose of collecting data from the serial object and storing 
-    the filtered data into a global variable.
-    The function has been put into a thread since the serial event is a blocking function.
-    """
     global serial_object
     global filter_data
-
     while(1):   
         try:
-            serial_data = serial_object.readline().strip('\n').strip('\r')
-            
+            serial_data = serial_object.readline().strip('\n').strip('\r')            
             filter_data = serial_data.split(',')
-            print filter_data
-        
+            print filter_data        
         except TypeError:
             pass
-    
-    
-        
 
+        
 def update_gui():
-    """" This function is an update function which is also threaded. The function assimilates the data
-    and applies it to it corresponding progress bar. The text box is also updated every couple of seconds.
-    A simple auto refresh function .after() could have been used, this has been avoid purposely due to 
-    various performance issues.
-    """
     global filter_data
     global update_period
 
@@ -133,12 +100,6 @@ def update_gui():
 
 
 def send():
-    """This function is for sending data from the computer to the host controller.
-    
-        The value entered in the the entry box is pushed to the UART. The data can be of any format, since
-        the data is always converted into ASCII, the receiving device has to convert the data into the required f
-        format.
-    """
     send_data = data_entry.get()
     
     if not send_data:
@@ -148,32 +109,14 @@ def send():
 
 
 
-def disconnect():
-    """ 
-    This function is for disconnecting and quitting the application.
-    Sometimes the application throws a couple of errors while it is being shut down, the fix isn't out yet
-    but will be pushed to the repo once done.
-    simple GUI.quit() calls.
-    """
-    try:
-        serial_object.close() 
-    
-    except AttributeError:
-        print "Closed without Using it -_-"
-
-    gui.quit()
-
-
+                                    #Main Loop
 
 if __name__ == "__main__":
-
-    """
-    The main loop consists of all the GUI objects and its placement.
-    The Main loop handles all the widget placements.
-    """
-    #frames
-    frame_1 = Frame(height = 285, width = 480, bd = 3, relief = 'groove').place(x = 7, y = 5)
-    frame_2 = Frame(height = 150, width = 480, bd = 3, relief = 'groove').place(x = 7, y = 300)
+    #frames   415x770
+    #frame_1 = Frame(height = 200, width = 377, bd = 3, relief = 'groove').place(x = 5, y = 5)
+    #frame_2 = Frame(height = 200, width = 377, bd = 3, relief = 'groove').place(x = 205, y = 5)
+    #frame_3 = Frame(height = 200, width = 377, bd = 3, relief = 'groove').place(x = 5, y = 382)
+    #frame_4 = Frame(height = 200, width = 377, bd = 3, relief = 'groove').place(x = 205, y = 382)
     text = Text(width = 65, height = 5)
 
     
@@ -190,10 +133,6 @@ if __name__ == "__main__":
     data4_ = Label(text = "Data4:").place(x = 15, y= 190)
     data5_ = Label(text = "Data5:").place(x = 15, y= 220)
 
-    baud   = Label(text = "Baud").place(x = 100, y = 348)
-    port   = Label(text = "Port").place(x = 200, y = 348)
-    contact = Label(text = "pratik.gurudatt@gmail.com").place(x = 250, y = 437)
-
     #progress_bars
     progress_1 = ttk.Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 200, max = 255)
     progress_2 = ttk.Progressbar(orient = HORIZONTAL, mode = 'determinate', length = 200, max = 255)
@@ -206,26 +145,16 @@ if __name__ == "__main__":
     #Entry
     data_entry = Entry()
     data_entry.place(x = 100, y = 255)
-    
-    baud_entry = Entry(width = 7)
-    baud_entry.place(x = 100, y = 365)
-    
-    port_entry = Entry(width = 7)
-    port_entry.place(x = 200, y = 365)
+
 
 
 
     #radio button
     button_var = IntVar()
-    radio_1 = Radiobutton(text = "Windows", variable = button_var, value = 1).place(x = 10, y = 315)
-    radio_2 = Radiobutton(text = "Linux", variable = button_var, value = 2).place(x = 110, y = 315)
-
     #button
-    button1 = Button(text = "Send", command = send, width = 6).place(x = 15, y = 250)
     connect = Button(text = "Connect", command = connect).place(x = 15, y = 360)
-    disconnect = Button(text = "Disconnect", command = disconnect).place(x =300, y = 360)
    
     #mainloop
-    gui.geometry('500x500')
+    gui.geometry('1024x600')
     gui.mainloop()
-    #this is a test
+    
